08e791ae7a07771feb108f149587656d1b1b816e
shahbazraza6006 <i201761@nu.edu.pk>
2024-03-31 00:29:55 +0500

    add

--- app/app.py
+++ app/app.py
@@ -2,13 +2,17 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pickle
 import numpy as np  # Import NumPy here
-
+import os
 # Initialize Flask app
 app = Flask(__name__)
 CORS(app)  # Enable CORS for the entire app
 
-# Load the trained model
-filename = 'movie_popularity_model.pkl'
+# Get the absolute path to the directory of the current script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Specify the absolute file path
+filename = os.path.join(current_dir, 'movie_popularity_model.pkl')
+
 model = pickle.load(open(filename, 'rb'))
 
 @app.route('/predict', methods=['POST'])
